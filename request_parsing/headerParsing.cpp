ad9edcd51592dc5f64ef7e5b13f10adf0bfd9bd4
9sammie <ballet.mathis@gmail.com>
2026-02-19 17:33:19 +0100

    parsing(headerParsing.cpp): the parsing of the request line is mostly done

--- request_parsing/headerParsing.cpp
+++ request_parsing/headerParsing.cpp
@@ -1,34 +1,70 @@
 #include "HttpParser.hpp"
 
+bool	checkPath(std::string path)
+{
+	if (path.empty() || path[0] != '/' || path.find(' ') != std::string::npos || path.find("..") != std::string::npos)
+		return 1;
+
+	for (size_t i = 0; i < path.size(); ++i)
+	{
+	    unsigned char c = path[i];
+	    if (std::iscntrl(c))
+	        return 1;
+	}
+
+	return 0;
+}
+
 HttpParser::ParseResult HttpParser::parseRequestLine(const std::string& requestLine, HttpRequest& request)
 {
-	size_t firstSpace = requestLine.find(' ');
+	size_t firstSpace;
+	size_t pathStart;
+	size_t secondSpace;
+	size_t versionStart;
+	size_t versionEnd;
+	std::string method;
+	std::string path;
+	std::string version;
+
+	firstSpace = requestLine.find(' ');
 	if (firstSpace == std::string::npos)
 		return ParseResult::INVALID_REQUEST_LINE;
-	size_t pathStart = requestLine.find_first_not_of(' ', firstSpace);
+	method = requestLine.substr(0, firstSpace);
+
+	pathStart = requestLine.find_first_not_of(' ', firstSpace);
 	if (pathStart == std::string::npos)
 	    return ParseResult::INVALID_REQUEST_LINE;
-
-	size_t secondSpace = requestLine.find(' ', firstSpace + 1);
+	secondSpace = requestLine.find(' ', firstSpace + 1);
 	if (secondSpace == std::string::npos)
 		return ParseResult::INVALID_REQUEST_LINE;
-	size_t versionStart = requestLine.find_first_not_of(' ', secondSpace);
+	path = requestLine.substr(pathStart, secondSpace - pathStart);
+
+	versionStart = requestLine.find_first_not_of(' ', secondSpace);
 	if (versionStart == std::string::npos)
 	    return ParseResult::INVALID_REQUEST_LINE;
-
-	size_t thirdSpace = requestLine.find(' ', secondSpace + 1);
-	if (thirdSpace == std::string::npos)
-		return ParseResult::INVALID_REQUEST_LINE;
-
-	std::string method = requestLine.substr(0, firstSpace);
-	std::string path = requestLine.substr(firstSpace + 1, secondSpace - firstSpace - 1);
-	std::string version = requestLine.substr(secondSpace + 1);
-
-	if (version != "HTTP/1.1")
-		return ParseResult::INVALID_HTTP_VERSION;
+	versionEnd = requestLine.find(' ', versionStart);
+	if (versionEnd == std::string::npos)
+	    version = requestLine.substr(versionStart);
+	else
+	{
+		if (requestLine.find_first_not_of(' ', versionEnd) != std::string::npos)
+			return ParseResult::INVALID_REQUEST_LINE;
+	    version = requestLine.substr(versionStart, versionEnd - versionStart);
+	}
 
 	if (method != "GET" && method != "POST" && method != "DELETE")
 		return ParseResult::INVALID_METHOD;
+	
+	if (checkPath(path))
+		return ParseResult::INVALID_URI;
+	
+	if (version != "HTTP/1.1")
+		return ParseResult::INVALID_HTTP_VERSION;
+
+	request.setMethod(method);
+	request.setPath(path);
+	request.setVersion(version);
+
 	return ParseResult::ALL_OK;
 }
 
